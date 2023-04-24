c90106af77b68a44b5546880e43c4cf3ac309539
dinhnt <dinhtruongnguyen11@gmail.com>
2023-04-24 09:47:13 +0700

    ok

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,15 +352,15 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to Chatbot UI
+            Welcome to Faavia
           </div>
-          <div className="text-center text-lg text-black dark:text-white">
+          {/* <div className="text-center text-lg text-black dark:text-white">
             <div className="mb-8">{`Chatbot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
             <div className="mb-2 font-bold">
               Important: Chatbot UI is 100% unaffiliated with OpenAI.
             </div>
-          </div>
-          <div className="text-center text-gray-500 dark:text-gray-400">
+          </div> */}
+          {/* <div className="text-center text-gray-500 dark:text-gray-400">
             <div className="mb-2">
               Chatbot UI allows you to plug in your API key to use this UI with
               their API.
@@ -385,7 +385,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                 openai.com
               </a>
             </div>
-          </div>
+          </div> */}
         </div>
       ) : modelError ? (
         <ErrorMessageDiv error={modelError} />
@@ -405,7 +405,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                         <Spinner size="16px" className="mx-auto" />
                       </div>
                     ) : (
-                      'Chatbot UI'
+                      'Faavia'
                     )}
                   </div>
 
--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -386,11 +386,11 @@ export const ChatInput = ({
           rel="noreferrer"
           className="underline"
         >
-          ChatBot UI
+          Faavia
         </a>
         .{' '}
         {t(
-          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
+          "Faavia is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
         )}
       </div>
     </div>
