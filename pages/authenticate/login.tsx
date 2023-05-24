34ad01627311df4b2e79fca8dfd1b9d2a56c4232
dinhtruongnguyen11 <48618511+dinhtruongnguyen11@users.noreply.github.com>
2023-05-24 16:55:46 +0700

    add authenticate pages

---
+++ pages/authenticate/login.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from '@/components/Form/LoginForm';
+
+export default function LoginPage() {
+  return (
+    <>
+      <div className="h-screen w-screen flex bg-[#F3F4F6] justify-center items-center">
+        <LoginForm />
+      </div>
+    </>
+  );
+}
