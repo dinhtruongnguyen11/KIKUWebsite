d821baa078f1584e0d0d39315f858b1c3e473d39
dinhtruongnguyen11 <48618511+dinhtruongnguyen11@users.noreply.github.com>
2023-06-03 08:25:31 +0700

    Update

--- pages/api/sendMail.ts
+++ pages/api/sendMail.ts
@@ -31,46 +31,36 @@ const sendOverNodeMailer = async (
       pass: process.env.MAIL_PASSWORD,
     },
   });
-  // await new Promise((resolve, reject) => {
-  //   transporter.verify(function (error, success) {
-  //     if (error) {
-  //       console.log(error);
-  //       reject(error);
-  //     } else {
-  //       console.log('Server is ready to take our messages');
-  //       resolve(success);
-  //     }
-  //   });
-  // });
+  await new Promise((resolve, reject) => {
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log('Server is ready to take our messages');
+        resolve(success);
+      }
+    });
+  });
+
   const mailOptions = {
     from: 'KIKU Team <info@kiku.do>',
     to,
     subject,
     html: content,
   };
-  await transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      console.error(err);
-      return false;
-    } else {
-      console.log(info);
-      return true;
-    }
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
   });
-  return true;
-  // console.log(1, res);
-  // await new Promise((resolve, reject) => {
-  //   // send mail
-  //   transporter.sendMail(mailOptions, (err, info) => {
-  //     if (err) {
-  //       console.error(err);
-  //       reject(err);
-  //     } else {
-  //       console.log(info);
-  //       resolve(info);
-  //     }
-  //   });
-  // });
 };
 
 export default async function handler(
@@ -84,15 +74,8 @@ export default async function handler(
   try {
     const { to, subject, content } = req.body;
 
-    sendOverNodeMailer(to, subject, content).then((rs) => {
-      if (rs) {
-        res.status(200).json({ message: 'Email sent successfully' });
-      } else {
-        res.status(500).json({
-          message: 'An error occurred while sending the email',
-        });
-      }
-    });
+    await sendOverNodeMailer(to, subject, content);
+    res.status(200).json({ message: 'Email sent successfully' });
   } catch (error) {
     res
       .status(500)
